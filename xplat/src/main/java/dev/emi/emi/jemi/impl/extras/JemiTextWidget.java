0778ded6e9525a6e90563aef81e1498e2ade7e28
LiquidCatMofu <131343315+liquidcatmofu@users.noreply.github.com>
2026-09-23 13:09:51 +0900

    Implement JEI 15.59 text widget tooltip API

--- xplat/src/main/java/dev/emi/emi/jemi/impl/extras/JemiTextWidget.java
+++ xplat/src/main/java/dev/emi/emi/jemi/impl/extras/JemiTextWidget.java
@@ -1,11 +1,14 @@
 package dev.emi.emi.jemi.impl.extras;
 
+import java.util.Collection;
 import java.util.List;
 
 import mezz.jei.api.gui.placement.HorizontalAlignment;
 import mezz.jei.api.gui.placement.VerticalAlignment;
+import mezz.jei.api.gui.widgets.IRecipeWidgetTooltipCallback;
 import mezz.jei.api.gui.widgets.ITextWidget;
 import net.minecraft.client.font.TextRenderer;
+import net.minecraft.client.item.TooltipData;
 import net.minecraft.text.StringVisitable;
 
 public class JemiTextWidget extends JemiPlaceable<ITextWidget> implements ITextWidget {
@@ -15,6 +18,7 @@ public class JemiTextWidget extends JemiPlaceable<ITextWidget> implements ITextW
 	public HorizontalAlignment horizontal = HorizontalAlignment.LEFT;
 	public VerticalAlignment vertical = VerticalAlignment.TOP;
 	public List<StringVisitable> text;
+	public IRecipeWidgetTooltipCallback tooltipCallback;
 
 	public JemiTextWidget(List<StringVisitable> text, int width, int height) {
 		super(width, height);
@@ -42,6 +46,31 @@ public class JemiTextWidget extends JemiPlaceable<ITextWidget> implements ITextW
 	@Override
 	public ITextWidget setShadow(boolean shadow) {
 		this.shadow = shadow;
+		return this;
+	}
+
+	@Override
+	public ITextWidget setTooltip(StringVisitable tooltip) {
+		this.tooltipCallback = builder -> builder.add(tooltip);
+		return this;
+	}
+
+	@Override
+	public ITextWidget setTooltip(Collection<? extends StringVisitable> tooltip) {
+		List<? extends StringVisitable> copy = List.copyOf(tooltip);
+		this.tooltipCallback = builder -> builder.addAll(copy);
+		return this;
+	}
+
+	@Override
+	public ITextWidget setTooltip(TooltipData tooltip) {
+		this.tooltipCallback = builder -> builder.add(tooltip);
+		return this;
+	}
+
+	@Override
+	public ITextWidget setTooltip(IRecipeWidgetTooltipCallback tooltipCallback) {
+		this.tooltipCallback = tooltipCallback;
 		return this;
 	}
 
