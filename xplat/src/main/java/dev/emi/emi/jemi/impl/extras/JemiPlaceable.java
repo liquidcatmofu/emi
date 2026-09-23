9ba295958767096d61614a06987603dd9c65d904
LiquidCatMofu <131343315+liquidcatmofu@users.noreply.github.com>
2026-09-23 13:09:44 +0900

    Implement aligned JEI placeable positioning

--- xplat/src/main/java/dev/emi/emi/jemi/impl/extras/JemiPlaceable.java
+++ xplat/src/main/java/dev/emi/emi/jemi/impl/extras/JemiPlaceable.java
@@ -1,6 +1,8 @@
 package dev.emi.emi.jemi.impl.extras;
 
+import mezz.jei.api.gui.placement.HorizontalAlignment;
 import mezz.jei.api.gui.placement.IPlaceable;
+import mezz.jei.api.gui.placement.VerticalAlignment;
 
 public class JemiPlaceable<T extends IPlaceable<T>> implements IPlaceable<T> {
 	public int x = 0, y = 0;
@@ -16,6 +18,14 @@ public class JemiPlaceable<T extends IPlaceable<T>> implements IPlaceable<T> {
 		this.x = xPos;
 		this.y = yPos;
 		return (T) this;
+	}
+
+	@Override
+	public T setPosition(int areaX, int areaY, int areaWidth, int areaHeight,
+			HorizontalAlignment horizontalAlignment, VerticalAlignment verticalAlignment) {
+		int xPos = areaX + horizontalAlignment.getXPos(areaWidth, getWidth());
+		int yPos = areaY + verticalAlignment.getYPos(areaHeight, getHeight());
+		return setPosition(xPos, yPos);
 	}
 
 	@Override
